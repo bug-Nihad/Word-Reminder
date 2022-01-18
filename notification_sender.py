162389623c76c3c275e4d02e5d0dcdd2bccc6dfd
bug-Nihad <tahsinsayed95@gmail.com>
2022-01-18 20:30:50 +0600

    Added to startup and some minor changes

--- notification_sender.py
+++ notification_sender.py
@@ -3,25 +3,25 @@ from plyer import notification
 import pandas as pd
 from random import randrange
 
+if __name__ == '__main__':
+    while True:
+        time.sleep(5*60)
+        try:
+            df = pd.read_excel('wordlist.xlsx')
+            df = df.fillna('Not Available')
+        except:
+            pass
 
-while True:
-    time.sleep(5)
-    try:
-        df = pd.read_excel('wordlist.xlsx')
-        df.fillna('Not Available')
-    except:
-        pass
+        # Getting number of row and selecting a random row
+        row = df.shape[0]
+        rand_row = randrange(0, row)
 
-    # Getting number of row and selecting a random row
-    row = df.shape[0]
-    rand_row = randrange(0, row)
-
-    # Send desktop notification
-    notification.notify(
-        title = str(df.iloc[rand_row, 0]) + ': ' + str(df.iloc[rand_row, 1]),
-        message = str(df.iloc[rand_row, 2]),
-        app_name = 'Word Reminder',
-        app_icon = 'dictionary.ico',
-        timeout = 100,
-        ticker='Hello'
-    )
+        # Send desktop notification
+        notification.notify(
+            title = str(df.iloc[rand_row, 0]) + ': ' + str(df.iloc[rand_row, 1]),
+            message = str(df.iloc[rand_row, 2]),
+            app_name = 'Word Reminder',
+            app_icon = 'dictionary.ico',
+            timeout = 10,
+            ticker='Hello'
+        )
